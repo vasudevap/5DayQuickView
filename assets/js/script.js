538da60128830f413de6aadb381f12edff5a05f6
Prashant <prashant.vasudeva@gmail.com>
2023-08-23 23:28:52 -0400

    all working excluding recents limit

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,16 +58,45 @@ var createSearchQuery = function (cityName) {
                   createWeatherCard(weatherData, todayDate);
                   saveCityToRecentSearches(cityName);
 
-                  var apiURL5Day = "https://api.openweathermap.org/data/2.5/forecast?lat="+ cityLat + "&lon=" + cityLon + "&appid=34188d1321d0f8500a6319995d20223e&units=imperial";
+                  var apiURL5Day = "https://api.openweathermap.org/data/2.5/forecast?cnt=121&lat="+ cityLat + "&lon=" + cityLon + "&appid=34188d1321d0f8500a6319995d20223e&units=imperial";
+
+                  // console.log(apiURL5Day);
 
                   fetch(apiURL5Day)
+
                     .then(function (response) {
+
                       if (response.ok) {
+
                         response.json().then(function (weatherForecastData) {
-                          create5DayWeatherCard(weatherForecastData);
+                          
+                          
+                          document.getElementById("fiveDayViewTitle").textContent = "5-Day Forecast:"
+                          for (var i=1; i<6; i++) {
+
+                            var nextDay = 8*i;
+
+                            // var timestampUnix = weatherForecastData.list[8*i];
+                            // console.log(convertUnixTimestamp(weatherForecastData.list[(8*i)][0]));
+                            // 1692846000
+                            // console.log(JSON.parse(weatherForecastData));
+                            // console.log(weatherForecastData.list[0]);
+                            // console.log(weatherForecastData.list[i].dt_txt);
+
+                            create5DayWeatherCard(weatherForecastData.list[nextDay-1], i);
+
+                          }
+                          
+
+
+                          // create5DayWeatherCard(forecastToShow);
+
                         })
+
                       } else {
+
                         alert("bad response: "+response.statusText);
+
                       }
 
                     });
@@ -104,13 +133,8 @@ var createWeatherCard = function (weatherData, forDate) {
   weatherIcon.setAttribute("src", weatherIconHref);
 
   document.getElementById("currentCityNameDateIcon").appendChild(weatherIcon);
-
-  // console.log(weatherIconHref);
-
   document.getElementById("currentCityTemp").textContent = "Temp: " + weatherData.main.temp + " ºF";
-
   document.getElementById("currentCityWind").textContent = "Wind: " + weatherData.wind.speed + " MPH";
-
   document.getElementById("currentCityHumidity").textContent = "Humidity: " + weatherData.main.humidity + " %";
 
 }
@@ -186,7 +210,6 @@ searchSubmitButtonEl.addEventListener('click', searchSubmitHandler);
 
 recentCityEl.addEventListener('click', function (event) {
 
-  // check if any of the recent city buttons was clicked
   event.preventDefault;
   // console.log(event.target);
 
@@ -195,15 +218,63 @@ recentCityEl.addEventListener('click', function (event) {
   // }
 
   if (recentCitiesInStorage.indexOf(event.target.name) >= 0) {
+    // a city was clicked from history
     recentsClickHandler(event.target.name);
   }
 
 });
 
-var create5DayWeatherCard = function (weatherForecastArray) {
-  console.log(weatherForecastArray);
+var create5DayWeatherCard = function (forecastDataArray, forecastDay) {
+
+  // console.log(forecastDataArray);
+
+  // resolve the icon
+  var weatherIconHref = "https://openweathermap.org/img/wn/" + forecastDataArray.weather[0].icon + "@2x.png";
+
+  // create Id for HTML elements to get
+  var divIdOfCard = "Day"+forecastDay+"Weather";
+  var pOfNameDate = divIdOfCard+"NameDate";
+  var pOfIcon = divIdOfCard+"Icon";
+  var pOfTemp = divIdOfCard+"Temp";
+  var pOfWind = divIdOfCard+"Wind";
+  var pOfHumidity = divIdOfCard+"Humidity";
+
+  var weatherIcon = document.createElement("img");
+
+  weatherIcon.setAttribute("class", "weatherIcon");
+  weatherIcon.setAttribute("alt", "weather icon");
+  weatherIcon.setAttribute("src", weatherIconHref);
+
+  // console.log(forecastDataArray.name+ " " + forecastDay + " ");
+
+  document.getElementById(pOfNameDate).textContent = forecastDataArray.dt_txt.slice(5,7)+"/"+forecastDataArray.dt_txt.slice(8,10)+"/"+forecastDataArray.dt_txt.slice(0,4);
+
+  document.getElementById(pOfIcon).appendChild(weatherIcon);
+  document.getElementById(pOfTemp).textContent = "Temp: " + forecastDataArray.main.temp + " ºF";
+  document.getElementById(pOfWind).textContent = "Wind: " + forecastDataArray.wind.speed + " MPH";
+  document.getElementById(pOfHumidity).textContent = "Humidity: " + forecastDataArray.main.humidity + " %";
+
 }
 
 if (recentCitiesInStorage) {
   displayRecents(JSON.parse(recentCitiesInStorage));
+}
+
+var convertUnixTimestamp = function(utcTime) {
+
+  var unix_timestamp = utcTime;
+  // Create a new JavaScript Date object based on the timestamp
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  var date = new Date(unix_timestamp * 1000);
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
+
+  // Will display time in 10:30:23 format
+  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+  console.log(formattedTime);
 }
